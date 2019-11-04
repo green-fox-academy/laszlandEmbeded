0fc2c831b71562a86e26a3eb1c9a6445905597ef
laszland <kiss.laszlo.roland@gmail.com>
2019-11-04 17:31:00 +0100

    finish Frequency

--- week-08/day-1/Blocking/GccApplication1/GccApplication1/main.c
+++ week-08/day-1/Blocking/GccApplication1/GccApplication1/main.c
@@ -9,37 +9,33 @@
 #define F_CPU 16000000
 #include <util/delay.h>
 
-
-
-void custom_delay_ms(int ms)
+void delay_time(int ms)
 {
 	for (int i = 0; i < ms; i++) {
 		_delay_ms(1);
 	}
 }
 
-void blink_at_mhz(int mhertz)
+void blink_at_mhrz(int mhertz)
 {
-	int delay_time = 500000 / mhertz;
+	int custom_delay_time = 400000 / mhertz;
 	PORTB = 1 << 5;
-	custom_delay_ms(delay_time);
+	delay_time(custom_delay_time);
 	PORTB = 0 << 5;
-	custom_delay_ms(delay_time);
+	delay_time(custom_delay_time);
 }
 
 
 int main(void)
 {
-    /* Replace with your application code */
-	
-	DDRB = 1 << 5;
-	int frequency[6] = {500, 1000, 2000, 4000, 2000, 1000};
-	
-    while (1) 
-    {
-		for (int i = 0; i < 6; i++) {
-			blink_at_mhz(frequency[i]);
-		}
-	}
+   DDRB = 1 << 5;
+   int frequency[6] = {1000, 2000, 4000, 8000, 2000, 1000};
+   
+   while(1)
+   {
+	   for (int i = 0; i < 6; i++) {
+		   blink_at_mhrz(frequency[i]);
+	   }
+   }
 }
 
