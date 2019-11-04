fe80048011e97622f6fc2735f8f99bb03dd56e7f
laszland <kiss.laszlo.roland@gmail.com>
2019-11-04 17:20:49 +0100

    finish Frequency play

--- week-08/day-1/Blocking/GccApplication1/GccApplication1/main.c
+++ week-08/day-1/Blocking/GccApplication1/GccApplication1/main.c
@@ -10,24 +10,35 @@
 #include <util/delay.h>
 
 
+
+void custom_delay_ms(int ms)
+{
+	for (int i = 0; i < ms; i++) {
+		_delay_ms(1);
+	}
+}
+
+void blink_at_mhz(int mhertz)
+{
+	int delay_time = 500000 / mhertz;
+	PORTB = 1 << 5;
+	custom_delay_ms(delay_time);
+	PORTB = 0 << 5;
+	custom_delay_ms(delay_time);
+}
+
+
 int main(void)
 {
     /* Replace with your application code */
 	
 	DDRB = 1 << 5;
-	uint8_t counter = 0;
-	PORTB = 0 << 5;
+	int frequency[6] = {500, 1000, 2000, 4000, 2000, 1000};
 	
     while (1) 
     {
-	
-		if (!(PINB & (1 << PINB7))) {
-			_delay_ms(300);
-			counter++;
-		}
-		
-		if (counter == 5) {
-			PORTB = 1 << 5;
+		for (int i = 0; i < 6; i++) {
+			blink_at_mhz(frequency[i]);
 		}
 	}
 }
