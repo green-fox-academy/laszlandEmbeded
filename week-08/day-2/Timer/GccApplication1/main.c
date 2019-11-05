9e806892f127951edcdef7fe7d5f2e7bb360da6a
laszland <kiss.laszlo.roland@gmail.com>
2019-11-05 17:21:38 +0100

    finish Timer with interrupt

--- week-08/day-2/Timer/GccApplication1/main.c
+++ week-08/day-2/Timer/GccApplication1/main.c
@@ -6,6 +6,19 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+uint8_t counter = 0;
+const uint8_t cntr_max = 10;
+
+ISR(TIMER0_OVF_vect)
+{
+	if (counter >= cntr_max) {
+		PINB = 1 << PINB5;
+		counter = 0;
+	}
+	counter++;
+}
 
 
 void init()
@@ -13,6 +26,8 @@ void init()
 	// Set the prescaler to 1024 division. See at the TC0 control register in the datasheet!
 	// With this you also set the clock source to CLK_io and you will also turn on the timer!
 	TCCR0B |= 0b00000101;
+	TIMSK0 |= 0x01;
+	sei();
 
 	// Set the PB5 LED pin to output
 	DDRB |= 1 << 5;
@@ -23,8 +38,7 @@ void init()
 int main(void)
 {
 
-	uint8_t counter = 0;
-	const uint8_t cntr_max = 30;
+	
 	
 	// Don't forget to call the init function!
 	init();
@@ -32,15 +46,7 @@ int main(void)
 	{
 		// If TC0 counter overflow occurred (TOV0 flag is set) toggle the LED and write "1" to that flag
 		// (this will clear it, it's confusing, but this is how it works)
-		if (TIFR0 & 0x01) {
-			counter++;
-			TIFR0 |= 0b00000001;
-		}
 		
-		if (counter >= cntr_max) {
-			PINB = 1 << PINB5;
-			counter = 0;
-		}
 	}
 }
 
