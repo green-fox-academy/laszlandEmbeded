48afba0f328928cb59a6937170dd267c52a463de
laszland <kiss.laszlo.roland@gmail.com>
2019-11-07 15:39:44 +0100

    LED dimming with joystick

--- week-08/day-4/ADC/GccApplication1/main.c
+++ week-08/day-4/ADC/GccApplication1/main.c
@@ -6,14 +6,39 @@
  */ 
 
 #include <avr/io.h>
-#include <stdio.h>
+#define F_CPU 16000000
+#include <util/delay.h>
+
+typedef enum pwm_type {
+	FAST_PWM,
+	PHASE_CORRECT
+} pwm_t;
+	
+void timer0_init(pwm_t type)
+{
+	switch (type) {
+		case FAST_PWM :
+		TCCR0A = 0b10000011;
+		TCCR0B = 0b00000010;
+		break;
+		case PHASE_CORRECT :
+		TCCR0A = 0b10000011;
+		TCCR0B = 0b00000010;
+		break;
+	}
+	DDRD |= 1 << DDRD6;
+}
 
 void init()
 {
+	ADMUX |= 1 << 5;		// left adjusted result
 	ADCSRA |= 0b11100000;
 	ADCSRB |= 0;
-	
-	DDRD |= 1 << DDRD6;
+}
+
+void set_duty(uint8_t duty_byte)
+{
+	OCR0A = duty_byte;
 }
 
 
@@ -21,6 +46,7 @@ int main(void)
 {
     /* Replace with your application code */
 	
+	timer0_init(PHASE_CORRECT);
 	init();
 	
 	uint8_t result1;
@@ -32,7 +58,12 @@ int main(void)
     {
 		result1 = ADCL;
 		result2 = ADCH;
-		combination_result = (((uint16_t)result2 << 8) | result1);
+		//combination_result = (((uint16_t)result2 << 8) | result1);
+		
+		set_duty(result2);
+		_delay_ms(100);
+		
+		
     }
 }
 
