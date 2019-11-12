02a1e67821acff22dd65aee40923848ece1e3d64
laszland <kiss.laszlo.roland@gmail.com>
2019-11-12 14:25:59 +0100

    starts project SudioH

---
+++ week-09/day-2/StudioH/StudioH/main.c
@@ -0,0 +1,18 @@
+/*
+ * StudioH.c
+ *
+ * Created: 11/12/2019 2:21:13 PM
+ * Author : Vaszja
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
