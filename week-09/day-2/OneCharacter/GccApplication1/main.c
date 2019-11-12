ecca9a2853a58007e550f38264f1b809d504a5dd
laszland <kiss.laszlo.roland@gmail.com>
2019-11-12 12:56:40 +0100

    creating new project: OneCharacter

---
+++ week-09/day-2/OneCharacter/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 11/12/2019 12:50:34 PM
+ * Author : Vaszja
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
